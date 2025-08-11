5860d45eb4153e53fee5bd449337066c894c507a
Ahmad Waqar <khawjaahmad@gmail.com>
2025-08-12 01:23:32 +0400

    update test bundle for dashboard test build

--- integration_test/test_bundle.dart
+++ integration_test/test_bundle.dart
@@ -9,7 +9,7 @@ import 'package:patrol/src/native/contracts/contracts.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'tests/accessibility_test.dart' as tests__accessibility_test;
+import 'tests/dashboard_test.dart' as tests__dashboard_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -69,7 +69,7 @@ Future<void> main() async {
   });
 
   // START: GENERATED TEST GROUPS
-  group('tests.accessibility_test', tests__accessibility_test.main);
+  group('tests.dashboard_test', tests__dashboard_test.main);
   // END: GENERATED TEST GROUPS
 
   final dartTestGroup = await testExplorationCompleter.future;
